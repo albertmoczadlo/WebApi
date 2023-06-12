0a166a0df2f83124a2666b47a7c5ad787a4f8b09
Albert <albertmoczadlo@gmail.com>
2023-06-12 10:05:27 +0200

    Added entities.

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,24 +1,23 @@
-namespace RestaurantAPI
+namespace RestaurantAPI;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+        // Add services to the container.
 
-            builder.Services.AddControllers();
+        builder.Services.AddControllers();
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+        // Configure the HTTP request pipeline.
 
-            app.UseHttpsRedirection(); 
+        app.UseHttpsRedirection(); 
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.Run();
-        }
+        app.Run();
     }
 }

dcbcf432786745c574a50ecb7651cfa0a0a1f001
Albert <albertmoczadlo@gmail.com>
2023-06-26 05:10:55 +0200

    Created first controller

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -15,8 +15,11 @@ public class Program
                 "RestaurantDbContextConnection")?? throw new InvalidOperationException("Connection not found")));
         builder.Services.AddScoped<RestaurantSeeder>();
         builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
+
+
 
         // Configure the HTTP request pipeline.
 
@@ -25,6 +28,18 @@ public class Program
             var seeder = scope.ServiceProvider.GetService<RestaurantSeeder>();
             seeder.Seed();
         }
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
 
         app.UseHttpsRedirection();
 
